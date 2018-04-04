8540a1868e1239ce5911db8be674d7c770e5dffe
Ruth1993 <ruth.scholten@gmail.com>
2018-04-04 10:32:04 +0200

    spaces and tabs fixed

--- client5.py
+++ client5.py
@@ -35,12 +35,12 @@ while True:
             if data.startswith(commands.START_POW) == True:
             	#server sent command to start proof of work assignment
                 print("jaaa, data starts with START POW")
-		
-            	z = int(data[len(commands.START_POW):].strip()) #take the remaining part of the command as the amount of zeros
-            	h = proofofwork.gen_attempt(z) #found hash h
-            	send_pow = name + " on ip " + commands.SEND_POW + commands.DELIM + h
-            	server.send(send_pow.encode())
-            elif data.startswith(commands.START_MINPOOL) == True:
+
+		z = int(data[len(commands.START_POW):].strip()) #take the remaining part of the command as the amount of zeros
+		h = proofofwork.gen_attempt(z) #found hash h
+		send_pow = name + " on ip " + commands.SEND_POW + commands.DELIM + h
+		server.send(send_pow.encode())
+	    elif data.startswith(commands.START_MINPOOL) == True:
                 print("minpool started")
 
 server.close()
